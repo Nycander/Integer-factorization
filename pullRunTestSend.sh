f03d371756bcdb435b6f69943099d5ff60d4541a
Martin Nycander <mnyc@kth.se>
2010-11-16 14:26:49 +0100

    OMG AWESOME TESTS

--- pullRunTestSend.sh
+++ pullRunTestSend.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-echo "Pulling latest revision from repository..."
+echo "! Pulling latest revision from repository..."
 
-# TODO: Pull from git
+git pull
 
-echo "Running make... "
+echo "! Compiling... "
 
-# TODO: Run "make"
+make -s
 
 number_of_tests=0
-failed_tests=1
 
-echo "Running tests... "
+echo "! Running tests... "
 
 # TODO: Run a bunch of tests
+./runTests.sh
+
+failed_tests=$?
 
 if [ $failed_tests = 0 ]; then
-	echo "Passed all tests! Sending to Kattis..."
+	echo "! Passed all tests! Sending to Kattis..."
 	# TODO: Send to kattis
 else
-	echo "Failed $failed_tests tests."
+	echo "! Failed $failed_tests tests."
 fi
-
-echo "Exiting."
--- runTests.sh
+++ runTests.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
 exec <tests.txt
-
+fails=0
 while read line
 do
-	echo $line
-	# TODO: Run program with input $line
-	# TODO: Check prime number
+	# Run program with input $line
+	output=$(echo "$line\\n^C" | ./factoring.exe)
+
+	result=1
+	resultstr="1"
+	for i in $output
+	do
+		# TODO: Check prime number
+		isPrime=$(echo $i | ./checkForPrime.bsh)
+		if [ "$isPrime" = "$i is not prime" ]; then
+			echo "\tFailed test '$line' because: $isPrime\n"
+			break
+		fi
+
+		resultstr="$resultstr*$i"
+		result=$(echo "$result*$i" | bc)
+	done
+
 	# TODO: Multiply output, check against input
+	if [ $result != $line ]; then
+		echo "\t Failed test '$line' because: ($resultstr=$result) != $line"
+		fails=$(echo "$fails+1" | bc)
+	fi
 done
+
+return $fails
