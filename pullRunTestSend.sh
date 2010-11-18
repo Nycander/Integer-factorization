ebf9c7816ee7c97e5d40b3b3bd8f5f90db0fd1d5
Martin Nycander <mnyc@kth.se>
2010-11-18 16:24:44 +0100

    Updated the script!

--- pullRunTestSend.sh
+++ pullRunTestSend.sh
@@ -19,7 +19,7 @@ number_of_tests=$(cat tests.txt | wc -l)
 if [ $failed_tests = 0 ]; then
 	echo "! Passed all $number_of_tests tests!"
 	# Send to kattis
-	yes | ./submit.py factoring.c > /dev/null
+	yes | ./submit.py -p factoring *.c *.h > /dev/null
 	echo "! Sent mail to kattis :D"
 else
 	echo "! Failed $failed_tests / $number_of_tests tests."
