87603bf287718b50c43078482ebb7e254da8d267
martin <martin.nycander@gmail.com>
2010-11-22 15:12:14 +0100

    Check for numbers <= 1 in trialdiv + includefix

--- trialdivision.c
+++ trialdivision.c
@@ -3,11 +3,19 @@
 
 #include "trialdivision.h"
 #include "factor_list.h"
+#include "primes.h"
 
 mpz_t * trial_division(factor_list ** f, int * primes, int primes_count, const mpz_t N)
 {
+
 	mpz_t * n = malloc(sizeof(mpz_t));
 	mpz_init_set(*n, N);
+
+	// Numbers below 2 should not be factored.
+	if (mpz_cmp_ui(N, 1) <= 0)
+	{
+		return n;
+	}
 
 	for(int i = 0; i < primes_count; i++)
 	{
