0491b90817565b31d7a10207e8f115d5e3a45587
martin <martin.nycander@gmail.com>
2010-11-24 00:49:19 +0100

    Gauss-elimination works now, at least on small test cases.

--- qs.c
+++ qs.c
@@ -123,6 +123,9 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 		#endif
 	}
 
+
+	// Time for some gauss!
+
 	#if VERBOSE
 	printf("\nComplete bit matrix:\n\n");
 	for(int column = 0; column < good_primes_count; column++)
@@ -135,36 +138,27 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 	}
 	#endif
 
-	#if VERBOSE
-	printf("\nMatrix will be %d x %d\n", number_count, good_primes_count);
-	#endif
 
 	// Initialize the real bit matrix
-	char bit_matrix[good_primes_count][number_count];
+	unsigned int bit_matrix_width = number_count;
+	unsigned int bit_matrix_height = good_primes_count;
+	#if VERBOSE
+		printf("\nMatrix will be %d x %d\n", bit_matrix_width, bit_matrix_height);
+	#endif
+	unsigned char bit_matrix[bit_matrix_height][bit_matrix_width];
 	// Copy values from complete_matrix to bit_matrix
 	int n = 0;
 	for(unsigned int i = 0; i < maxNumberOfSieving; i++) // numbers to factorize
 	{
 		if (mpz_cmp_ui(numbers[i], 1) == 0 && mpz_cmp_ui(copy[i], 1) != 0)
 		{
-			for(unsigned int p = 0; p < good_primes_count; p++)
+			for(unsigned int p = 0; p < bit_matrix_height; p++)
 			{
 				bit_matrix[p][n] = complete_bit_matrix[p][i];
 			}
 			n++;
 		}
 	}
-
-	#if VERBOSE
-	for(int column = 0; column < good_primes_count; column++)
-	{
-		for(int row = 0; row < number_count; row++)
-		{
-			printf("%d ", bit_matrix[column][row]);
-		}
-		printf("\n");
-	}
-	#endif
 
 	// Clear our variables!
 	mpz_clear(sqrtN), mpz_clear(tmp), mpz_clear(mod);
@@ -174,26 +168,41 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 		mpz_clear(copy[i]);
 	}
 
-	// Guass elminiation
-	for(int column = 0, row = 0; column < number_count; column++, row++)
+	#if VERBOSE
+	for(int column = 0; column < bit_matrix_height; column++)
+	{
+		for(int row = 0; row < bit_matrix_width; row++)
+		{
+			printf("%d ", bit_matrix[column][row]);
+		}
+		printf("\n");
+	}
+	#endif
+
+	// Gauss elimination
+	for(int column = 0, row = 0; column < bit_matrix_width; column++, row++)
 	{
 		#if VERBOSE
-		printf("\n\tLooking at column %d amd row %d in matrix:\n", column, row);
-		for(int row = 0; row < good_primes_count; row++)
+		printf("\n\tLooking at column %d and row %d in matrix:\n", column, row);
+
+		if (bit_matrix_height < 100 && bit_matrix_width < 100)
 		{
-			printf("\t");
-			for(int column = 0; column < number_count; column++)
+			for(int column = 0; column < bit_matrix_height; column++)
 			{
-				printf("%d ", bit_matrix[row][column]);
+				printf("\t");
+				for(int row = 0; row < bit_matrix_width; row++)
+				{
+					printf("%d ", bit_matrix[column][row]);
+				}
+				printf("\n");
 			}
 			printf("\n");
 		}
-		printf("\n");
 		#endif
 
 		// Find first 1 in column
 		int maxRow = row;
-		while(maxRow < good_primes_count-1)
+		while(maxRow < bit_matrix_height-1)
 		{
 			if (bit_matrix[maxRow][column] == 1)
 				break;
@@ -221,7 +230,7 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 
 			// Swap row i and maxColumn
 			char tmp[maxNumberOfSieving];
-			for(int c = 0; c < number_count; c++)
+			for(int c = 0; c < bit_matrix_width; c++)
 			{
 				tmp[c] = bit_matrix[row][c];
 				bit_matrix[row][c] = bit_matrix[maxRow][c];
@@ -234,7 +243,7 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 		#endif
 
 		// Make sure all rows below this row has an initial zero.
-		for(int r = row+1; r < good_primes_count; r++)
+		for(int r = row+1; r < bit_matrix_height; r++)
 		{
 			if (bit_matrix[r][column] == 0)
 				continue;
@@ -244,7 +253,7 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 			#endif
 
 			// Subtract bit_matrix[k][row] * bit_matrix[column] from bit_matrix[k]
-			for(int c = 0; c < number_count; c++)
+			for(int c = 0; c < bit_matrix_width; c++)
 			{
 				bit_matrix[r][c] = bit_matrix[r][c] ^ bit_matrix[row][c];
 			}
@@ -255,9 +264,9 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 	}
 	#if VERBOSE
 	printf("\nAfter gauss:\n\n");
-	for(int column = 0; column < good_primes_count; column++)
+	for(int column = 0; column < bit_matrix_height; column++)
 	{
-		for(int row = 0; row < number_count; row++)
+		for(int row = 0; row < bit_matrix_width; row++)
 		{
 			printf("%d ", bit_matrix[column][row]);
 		}
@@ -265,9 +274,93 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 	}
 	#endif
 
-	// TODO: Get solution vectors
+	int known = 0;
+	int knownIndexes[bit_matrix_width];
+	for(int i = 0; i < bit_matrix_width; i++)
+		knownIndexes[i] = -1;
 
-	// TODO: Use epic math to calculate factors.
+	for(int y = 0; y < bit_matrix_height; y++)
+	{
+		int x = 0;
+		while(x < bit_matrix_width)
+		{
+			if (bit_matrix[y][x] == 1)
+			{
+				break;
+			}
+			x++;
+		}
+
+		if (x < bit_matrix_width)
+		{
+			knownIndexes[x] = x;
+			known++;
+		}
+	}
+
+	#if VERBOSE
+	printf("\nWe have %d known variables, thus there are %d unknowns\n", known, bit_matrix_width-known);
+	#endif
+
+	int unknowns = bit_matrix_width-known;
+
+	// For all 2^unknowns permutations
+	for(int i = 0; i < (1 << unknowns); i++)
+	{
+		// Idea: i can be used with masks to get the current value of the unknowns.
+		char solution[bit_matrix_width];
+
+		// Fill the unknowns first
+		int unknown_cntr = 0;
+		for(int s = 0; s < bit_matrix_width; s++)
+		{
+			if (knownIndexes[s] != s)
+			{
+				solution[s] = (i & (1 << unknown_cntr)) >> unknown_cntr;
+				unknown_cntr++;
+			}
+			else
+			{
+				solution[s] = -1;
+			}
+		}
+
+		// Fill the knowns bottom up
+		for(int s = bit_matrix_width-1; s >= 0; s--)
+		{
+			if (solution[s] != -1)
+				continue;
+
+			solution[s] = 0;
+
+			// Search the column for the "1" (should be only 1)
+			int row = -1;
+			for(int y = 0; y < bit_matrix_height; y++)
+			{
+				if (bit_matrix[y][s] == 1)
+				{
+					row = y;
+					break;
+				}
+			}
+
+			// Calculate value from equation vector
+			for(int x = s; x < bit_matrix_width; x++)
+			{
+				solution[s] = solution[s] ^ bit_matrix[row][x];
+			}
+		}
+
+		#if VERBOSE
+		printf("Solution array: ");
+		for(int s = 0; s < bit_matrix_width; s++)
+		{
+			printf("%d ", solution[s]);
+		}
+		printf("\n");
+		#endif
+	}
+
 
 	return 0;
 }
