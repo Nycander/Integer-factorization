9f455d1c03f635d14103976fb821a1ca992d8e95
Hirschen <hirsimaa@gmail.com>
2010-11-28 23:08:47 +0100

    added clear for nums, nums_p

--- qs.c
+++ qs.c
@@ -440,6 +440,11 @@ int quadratic_sieve(factor_list ** result, const mpz_t num)
 		}
 	}
 
+	for(int s = 0; s < bit_matrix_width; s++){
+		mpz_clear(nums[s]);
+		mpz_clear(nums_p[s]);
+	}
+
 	// Clear our variables!
 	mpz_clear(sqrtN), mpz_clear(ret1), mpz_clear(ret2), mpz_clear(tmp), mpz_clear(mod);
 
