617fe1d1a8b0caa01a47ab7e81cd7207fe94b242
Martin Nycander <mnyc@kth.se>
2010-11-29 03:58:07 +0100

    Oh lol, QS never runs @ kattis, but this seems to work :)

--- settings.h
+++ settings.h
@@ -4,14 +4,14 @@
 // Global settings
 #define VERBOSE 0
 #define USE_TRIAL_DIVISION 1
-#define USE_QUADRATIC_SIEVE_BIT_THRESHOLD 30
+#define USE_QUADRATIC_SIEVE_BIT_THRESHOLD 40
 
 // Pollard's Rho settings
-#define POLLARD_THRESHOLD 250000
+#define POLLARD_THRESHOLD 150000
 #define USE_BRENT_INSTEAD_OF_FLOYD 0
 
 
 // Quadratic sieve settings
-#define MAX_NUMBER_OF_SOLUTION_VECTORS 16 // 2^(number)
+#define MAX_NUMBER_OF_SOLUTION_VECTORS 12 // 2^(number)
 
 #endif
