03f0bd40f2614d959ef7c64b7e5e5ac97b0bee98
Christoffer Hirsimaa <chir@nada.kth.se>
2010-11-29 10:11:52 +0100

    kattis

--- settings.h
+++ settings.h
@@ -3,12 +3,12 @@
 
 // Global settings
 #define VERBOSE 0
-#define USE_TRIAL_DIVISION 0
-#define USE_QUADRATIC_SIEVE_BIT_THRESHOLD 10
+#define USE_TRIAL_DIVISION 1
+#define USE_QUADRATIC_SIEVE_BIT_THRESHOLD 50
 
 // Pollard's Rho settings
-#define POLLARD_THRESHOLD 150000
-#define USE_BRENT_INSTEAD_OF_FLOYD 0
+#define POLLARD_THRESHOLD 350000
+#define USE_BRENT_INSTEAD_OF_FLOYD 1
 
 
 // Quadratic sieve settings
